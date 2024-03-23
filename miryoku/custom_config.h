953cb75552bc80679bc7a79712a8752ef0028e57
gogamid <36050790+gogamid@users.noreply.github.com>
2024-03-23 15:40:55 +0100

    mouse speed 1

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,15 +6,3 @@
 
 // mouse support caksoylar/zmk/feat/mouse-keys-3.2
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
-
-// TODO: test if this works, if not  then change these in
-// miryoku_kludge_mousekeyspr.h
-//
-// Both mouse movement and scrolling have independently configurable
-// acceleration profiles with three parameters: delay before movement, time to
-// max speed and the acceleration exponent. The exponent is usually set to 0 for
-// constant speed, 1 for uniform acceleration or 2 for uniform jerk.
-#define U_MOUSE_MOVE_MAX 2500
-#define U_MOUSE_MOVE_EXPONENT 1
-#define U_MOUSE_MOVE_TIME 1500
-#define U_MOUSE_MOVE_DELAY 0
