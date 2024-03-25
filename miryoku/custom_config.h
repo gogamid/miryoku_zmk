feedb3c21c74a21cd2dcd3ee39bef507c2632fb3
gogamid <36050790+gogamid@users.noreply.github.com>
2024-03-25 15:18:37 +0100

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -7,7 +7,7 @@
 // mouse support caksoylar/zmk/feat/mouse-keys-3.2
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
-#define MIRYOKU_LAYERS_MOUSE \
+#define MIRYOKU_LAYER_MOUSE \
 U_WH_U,            U_WH_L,            U_MS_U,            U_WH_R,            U_NU,              U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
 U_WH_D,            U_MS_L,            U_MS_D,            U_MS_R,            U_NU,              &kp K_SLEEP,       &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             U_NA,              &u_to_U_MOUSE,     &u_to_U_SYM,       &kp RALT,          U_NA,              \
